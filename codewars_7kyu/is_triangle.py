7f10cfd17946146716be0ab4d2b9f94544b848a9
AniaPotrykus <anmapotrykus@gmail.com>
2024-08-04 17:33:00 +0200

    add improvements

--- codewars_7kyu/is_triangle.py
+++ codewars_7kyu/is_triangle.py
@@ -2,6 +2,8 @@
 #of given length and false in any other case.
 
 def is_triangle(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return False
     if a + b > c and a + c > b and b + c > a:
         return True
     else:
