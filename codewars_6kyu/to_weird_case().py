39f5e51841998b1ee972544bcf0c4b018dd04762
AniaPotrykus <anmapotrykus@gmail.com>
2024-08-28 09:26:07 +0200

    add to_weird_case().py

---
+++ codewars_6kyu/to_weird_case().py
@@ -0,0 +1,22 @@
+#Write a function that accepts a string, and returns the same string with all even indexed characters in each word upper cased, and 
+#all odd indexed characters in each word lower cased. The indexing just explained is zero based, so the zero-ith index is even, 
+#therefore that character should be upper cased and you need to start over for each word.
+
+#The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are multiple 
+#words. Words will be separated by a single space(' ').
+
+#Examples:
+#"String" => "StRiNg"
+#"Weird string case" => "WeIrD StRiNg CaSe"
+
+def to_weird_case(words):
+    word_list = words.split()
+    result = []
+    for word in word_list:
+      for i, el in enumerate(word):
+          if i % 2 == 0:
+            result.append(el.upper())
+          else:
+            result.append(el.lower())
+      result.append(" ")
+    return "".join(result).strip()
